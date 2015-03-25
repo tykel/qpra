24c1dfdd665acb8c0b1124e5941e37936cdff5a5
Tim Kelsall <timothy.kelsall@gmail.com>
2015-03-25 01:30:21 -0700

    Updated palette and test program

--- test.s
+++ test.s
@@ -1,7 +1,26 @@
+; Draw a single sprite on the screen.
+
 .bank rom_fixed
 
-init:   mv a, 1
-test:   jz loop
-        mv a, 0
-        jp test
-loop:   jp loop
+init:       mv a, $80           ; sprite Enable bit
+            mv.b [$ea00], a     ; update sprite 0 mmr
+            mv a, $0101         ; x = 1, y = 1
+            mv [$eb00], a       ; update group 0 pos.
+            mv a, 0             ; use palette 0
+            mv [$eb81], a       ; update sprite palette mmr
+            mv a, $ff           ; color white in global palette
+            mv.b [$e901], a     ; update palette 0, entry 1
+
+loop:       jp loop
+
+.bank tile_swap 0
+
+.db $11,$11,$11,$11,
+.db $11,$00,$11,$11,
+.db $11,$00,$11,$11,
+.db $11,$00,$11,$11,
+.db $11,$11,$00,$11,
+.db $11,$11,$00,$11,
+.db $11,$11,$00,$11,
+.db $11,$11,$11,$11,
+
